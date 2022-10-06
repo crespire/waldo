4aa9567044c248207f38b7eafc8fb0b6e7372375
crespire <li.simmon@gmail.com>
2022-10-05 21:42:44 -0400

    feat: set up image canvas
    * Renames Image to ImageArea
    * Sets up loading of image for drawing
    * Introduces state to record start time

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,14 +1,16 @@
+import { useState } from 'react';
 import { useParams } from "react-router-dom";
 import GameInfo from "./GameInfo";
-import Image from "./Image";
+import ImageArea from "./ImageArea";
 
 function Game(props) {
   const params = useParams();
+  const [startTime, setStartTime] = useState(0);
 
   return(
     <div className="flex flex-col w-full h-full">
       <GameInfo />
-      <Image image={params['image']} />
+      <ImageArea image={params['image']} setStartTime={setStartTime} />
     </div>
   );
 }
---
+++ client/src/components/ImageArea.js
@@ -0,0 +1,37 @@
+import { useEffect } from 'react';
+
+function ImageArea(props) {
+  const { image, setStartTime } = props;
+
+  const handleClick = (event) => {
+    const x = event.nativeEvent.layerX;
+    const y = event.nativeEvent.layerY;
+    console.log(`X: ${x}, Y: ${y}`);
+
+    // Send backend x, y and image. Then we can check if the click is within bounds for any character
+    // If so, send back x, y to make circle on image.
+  }
+  
+  useEffect(() => {
+    const img = new Image();
+    img.src = require(`../assets/images/${image}.jpg`);
+    const canvas = document.getElementById('image-canvas');
+    const ctx = canvas.getContext('2d');
+    img.onload = () => {
+      ctx.drawImage(img, 0, 0);
+      setStartTime(Date.now());
+    }    
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return(
+    <div id="play-area" className="basis-full h-full w-full overflow-auto">
+      <canvas onClick={handleClick} id="image-canvas" width="3000" height="1920"></canvas>
+      {
+        // <img onClick={handleClick} className="min-w-min min-h-min" src={require(`../assets/images/${image}.jpg`)} alt={image}></img>
+      }
+    </div>
+  );
+}
+
+export default ImageArea;
