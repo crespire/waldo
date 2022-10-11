67bd73b5547c2987b66f5c0ab9b88bb0779d6e51
crespire <li.simmon@gmail.com>
2022-10-11 16:39:14 -0400

    fix: update callback to accept boolean

--- client/src/components/WinModal.js
+++ client/src/components/WinModal.js
@@ -2,6 +2,7 @@ import ReactPortal from "./ReactPortal";
 import { useState, useEffect, useCallback, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { ImageContext } from "./Game";
+import Close from '../assets/images/close.svg';
 import useForm from "../hooks/useForm";
 
 function WinModal(props) {
@@ -11,14 +12,15 @@ function WinModal(props) {
   const [leaderboard, setLeaderboard] = useState({});
   let navigate = useNavigate();
 
-  const closeCallback = useCallback(
-    () => {
-      console.log('Submitted to backend!');
-      navigate('/');
-      setModalOpen(!modalOpen);
-    },
-    [setModalOpen, modalOpen, startTime, endTime]
-  );
+  const closeCallback = (_, submitted = false) => {
+    if (values['name'] && submitted) {
+      console.log('Submitted to backend with ', values['name']);
+    } else {
+      console.log('Skipped score submit');
+    }
+    navigate('/');
+    setModalOpen(!modalOpen);
+  };
 
   const { values, errors, handleChange, handleSubmit } = useForm(closeCallback);
 
@@ -29,7 +31,8 @@ function WinModal(props) {
     return () => {
       document.body.removeEventListener("keydown", closeOnEscapeKey);
     };
-  }, [closeCallback]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [modalOpen]);
 
   // Load leaderboard info
   useEffect(() => {
@@ -49,14 +52,16 @@ function WinModal(props) {
 
   return(
     <ReactPortal className="absolute bg-alpha-white flex justify-center items-center top-0 left-0 w-screen h-screen" wrapperID="portal-container">
+      <button className="flex border border-black rounded-full border-solid p-2 bg-white mr-2" onClick={closeCallback}><img src={Close} alt="X"></img></button>
       <div className="flex flex-col space-y-2 bg-white border border-black border-solid p-4">
         <h1 className="text-2xl">You found them all!</h1>
         <p>Your search took {secondsToComplete} seconds.</p>
-        <form className="flex flex-col p-2 border border-solid border-black space-y-2" onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="name">Name: </label>
-            <input className="border border-slate-400 border-solid" type="text" name="name" value={values['name'] || ''} onChange={handleChange} placeholder="AAA" required></input>
-            { errors.name && <p className="border border-rose-500 border-solid">{errors.name}</p>}
+        <p>Add your score, or try again!</p>
+        <form className="flex flex-col p-2 border border-solid border-black space-y-2" onSubmit={(e) => {handleSubmit(e, true)}}>
+          <div className="grow">
+            <label className="grow-0" htmlFor="name">Name: </label>
+            <input className="grow border border-slate-400 border-solid" type="text" name="name" value={values['name'] || ''} onChange={handleChange} placeholder="AAA" required></input>
+            { errors.name && <p className="text-center border border-rose-500 border-solid">{errors.name}</p>}
           </div>
           <button className="border border-solid border-black" type="submit">Add your score!</button>
         </form>
