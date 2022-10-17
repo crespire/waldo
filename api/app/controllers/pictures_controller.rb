c2295ee67884662b64a7a6a16f8b9fb4a58662dd
crespire <li.simmon@gmail.com>
2022-10-17 18:36:13 -0400

    fix: prettify pictures index json

--- api/app/controllers/pictures_controller.rb
+++ api/app/controllers/pictures_controller.rb
@@ -1,6 +1,6 @@
 class PicturesController < ApplicationController
   def index
-    @pictures = Picture.all
+    @pictures = Picture.all.pluck(:name, :difficulty).map { |name, difficulty| { name: name, difficulty: difficulty} }
     render json: @pictures
   end
 end
