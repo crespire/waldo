5d742cec54d19715c950b532756507c9e62a5f4a
crespire <li.simmon@gmail.com>
2022-10-17 18:19:26 -0400

    feat: add track seed data to db

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Picture.create(name: 'track', difficulty: 'easy')
+Picture.first.secrets.build(name: 'waldo', coords: [844, 640]).save
+Picture.first.secrets.build(name: 'wilma', coords: [750, 1380]).save
+Picture.first.secrets.build(name: 'wizard', coords: [1840, 1640]).save
+Picture.first.secrets.build(name: 'oddlaw', coords: [1800, 1230]).save
