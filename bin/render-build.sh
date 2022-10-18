6dde3b96412cedc063a70ab5967cacbdf3f94633
crespire <li.simmon@gmail.com>
2022-10-18 09:34:16 -0400

    fix: update render build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 # exit on error
-cd api && set -o errexit
+cd api/ && set -o errexit
 
-cd api && bundle install
-cd api && bundle exec rake assets:precompile
-cd api && bundle exec rake assets:clean
-cd api && bundle exec rake db:migrate
+cd api/ && bundle update -- bundler
+cd api/ && bundle install
+cd api/ && bundle exec rake assets:precompile
+cd api/ && bundle exec rake assets:clean
+cd api/ && bundle exec rake db:migrate
+cd api/ && bundle exec rake db:seed
