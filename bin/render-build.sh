82ffc6e23ddaf58a1519ef257673d438f5f12244
crespire <li.simmon@gmail.com>
2022-10-18 09:52:16 -0400

    fix: add bundler

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # exit on error
+gem install bundler
+
 cd api
 set -o errexit
-
-gem install bundler
 bundle update -- bundler
 bundle install
 bundle exec rake assets:precompile
