35fbf1af4cc01d042cd21ae405f957e07055a0c1
crespire <li.simmon@gmail.com>
2022-10-18 10:18:25 -0400

    fix: remove asset build steps

--- api/bin/render-build.sh
+++ api/bin/render-build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
+# bundle exec rake assets:precompile - API application, no assets.
+# bundle exec rake assets:clean
 bundle exec rake db:migrate
 bundle exec rake db:seed
