ed464c07b722d306a82dcb872a834df14bfd6d6b
crespire <li.simmon@gmail.com>
2022-10-23 19:33:51 -0400

    fix: add new host URL

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -9,10 +9,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     #origins 'http://localhost:3001', '127.0.0.1:3001'
 
-    origins 'crespire-waldo.vercel.app'
+    origins 'waldo.crespire.dev', 'crespire-waldo.vercel.app'
     resource '*', headers: :any, methods: %i[get post]
   end
 end
 
+Rails.application.config.hosts << 'waldo.crespire.dev'
 Rails.application.config.hosts << 'crespire-waldo.vercel.app'
 Rails.application.config.hosts << 'waldo-backend.onrender.com'
