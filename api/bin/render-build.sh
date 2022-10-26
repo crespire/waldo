0fe0a528fff8966f1eda0034decfdfe18d9840bd
crespire <li.simmon@gmail.com>
2022-10-26 11:47:17 -0400

    fix: remove render build files

--- api/bin/render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-bundle install
-# bundle exec rake assets:precompile - API application, no assets.
-# bundle exec rake assets:clean
-bundle exec rake db:migrate
-bundle exec rake db:seed
